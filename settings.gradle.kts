f2256509a1a32676de51d7394b7b1ee655c49340
vinz2112 <hippiefishmalta@gmail.com>
2023-03-26 14:12:11 +0200

    1st-initial commit-push

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "fone_kotlin_multiplatform_app"
+include(":androidApp")
+include(":shared")
